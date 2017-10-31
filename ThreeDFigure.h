8af0adf65d1429a14c91bb0e101e33631761754c
Artyom Andreev <aandreev06.1998@gmail.com>
2017-10-31 19:44:06 +0300

    virtual ~ {}

--- ThreeDFigure.h
+++ ThreeDFigure.h
@@ -6,7 +6,7 @@ public:
     virtual void about() const = 0;
     virtual double get_volume() const = 0;
     virtual double get_surface_area() const = 0;
-    virtual ~ThreeDFigure() = 0;
+    virtual ~ThreeDFigure() {};
 };
 
 #endif //C_TASK6_THREEDFIGURE_H
